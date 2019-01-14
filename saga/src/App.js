a7b868fcd3082e2d15a73ebd75035ef892c82a0a
mengxxSELF <2473536678@qq.com>
2019-01-14 20:11:30 +0800

    saga

--- saga/src/App.js
+++ saga/src/App.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
+import logo from './logo.svg';
 import './App.css';
 import Main from './main'
-import { Provider } from 'react-redux'
-import { store} from './redux/store'
-console.log(store.getState())
+// import { Provider } from 'react-redux'
+// import { store } from './redux/store'
+// console.log(store.getState())
 class App extends Component {
   render() {
     return (
-      <Provider store={store}>
-        <Main />
-      </Provider>
+      <div className="App">
+        <header className="App-header">
+          <div> abcd </div>
+        </header>
+      </div>
     );
   }
 }
