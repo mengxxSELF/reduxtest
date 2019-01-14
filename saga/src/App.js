a015f14561831c00f4fae2c88955b31b8c620784
mengxxSELF <2473536678@qq.com>
2019-01-14 19:42:52 +0800

    saga

---
+++ saga/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+import Main from './main'
+import { Provider } from 'react-redux'
+import { store} from './redux/store'
+console.log(store.getState())
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <Main />
+      </Provider>
+    );
+  }
+}
+
+export default App;
